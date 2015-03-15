1c059c49f6986399a879b140fb6300f192e5b681
Alex Albino <webmaster@alexventure.com>
2015-03-14 20:52:59 -0700

    Adding single and page view PHP files

--- home-posts.php
+++ home-posts.php
@@ -8,23 +8,12 @@
 <?php
 
  foreach($recentPosts as $recentPost):
+ 	$post = $recentPost;
+
+ 	get_template_part('articlepost');
 ?>
 	
-	<article class="type-system-sans recent-post">
-		
-		<h2 id="<?php echo $recentPost->post_type ?>-<?php echo $recentPost->ID ?>">
-			<a href="<?php echo get_permalink($recentPost->ID); ?>">
-			<?php echo apply_filters('the_title', $recentPost->post_title); ?>
-			</a>
-		</h2>
-		<p class="date"><?php echo get_the_date( null, $recentPost->ID) ?></p>
-      	<p>
-	      	<?php echo $recentPost->post_content; ?>
-      	</p>
-      	<hr>
-      	<p class="author"><?php the_author_meta('user_nicename', $recentPost->post_author); ?></p>
-      	
-	</article>
+	
 	<hr>
 <?php 
  endforeach; ?>
--- page.php
+++ page.php
@@ -3,6 +3,28 @@
 /**
  * Page
  */
+global $post;
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <?php get_header() ?>
+    <body>
 
-?>
+        <?php get_template_part('navigation') ?>
+
+        <div class="main-container">
+
+            <div class="body-container">
+            
+                <?php get_template_part('articlepost'); ?>
+                
+                <?php get_sidebar( 'frontpage' ); ?>
+
+            </div>
+        </div>
+
+        <?php get_template_part('footer-navigation'); ?>
+        <?php get_footer() ?>
+
+    </body>
+</html>
 <!-- Page -->
